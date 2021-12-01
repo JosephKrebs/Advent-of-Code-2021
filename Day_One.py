b2e87ceacc9044646389f02d981479d046eab9fb
JosephKrebs <58876030+JosephKrebs@users.noreply.github.com>
2021-12-02 01:18:51 +1000

    Completed part two.

--- Day_One.py
+++ Day_One.py
@@ -1,24 +1,37 @@
 def part_one():
-
     file = open("input.txt", "r")
-
     lines = []
-    conds = []
-    count = 0
+    conditional_array = []
+
     for line in file:
         lines.append(int(line))
 
     for i in range(len(lines)):
         if i != 0:
             if lines[i] > lines[i-1]:
-                conds.append(True)
+                conditional_array.append(True)
             else:
-                conds.append(False)
+                conditional_array.append(False)
 
-    for i in conds:
+    count = 0
+    for i in conditional_array:
         if i is True:
             count += 1
 
+    print(count)
+
+def part_two():
+    file = open("input.txt", "r")
+    lines = []
+
+    for line in file:
+        lines.append(int(line))
+
+    block = [lines[i:i+3] for i in range(len(lines) - 2)]
+    count = 0
+    for f, b in zip(block[:-1], block[1:]):
+        if sum(f) < sum(b):
+            count += 1
     print(count)
 
 def main():
